f46874932830564d44e528f2a666e770b46587eb
zaniu91 <864880660@qq.com>
2023-07-05 10:16:17 +0800

    Update basic.vue

--- docs/examples/table/basic.vue
+++ docs/examples/table/basic.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
+  <el-table :data="tableData" style="width: 100%" flexible>
     <el-table-column prop="date" label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" />
